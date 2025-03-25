50dbc404217f6822c136f42c1f56e297c0e4e708
freshbeer12 <nekvoj13@gasos-ro.cz>
2025-03-25 08:19:05 +0100

    zarizeni

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,59 @@
 import java.util.Scanner;
+
 public class Main {
     static Scanner sc = new Scanner(System.in);
-public static void main(String[] args) {
 
-    for (int k = 0; k == 0; k--){
+    public static void main(String[] args) {
+        System.out.println("Kolik zařízení chceš zadat?");
+        int count = sc.nextInt();
+        sc.nextLine();
 
-        System.out.println("Chceš zapisovat (1) nebo zobrazovat (2) ?");
-    int vyber = sc.nextInt();
-    sc.nextLine();
-    switch (vyber) {
-        case 1:
+        IPrehravac[] prehravace = new IPrehravac[count];
 
-
-            System.out.println("Kolik zařízení chceš zadat?");
-            int count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            System.out.println("Stiskni 1 pro MP3 ; Stiskni 2 pro Smartphone ; Stiskni 3 pro Radio");
+            int volba = sc.nextInt();
             sc.nextLine();
 
-            IPrehravac[] prehravace = new IPrehravac[count];
-
-            for (int i = 0; i < count; i++) {
-                System.out.println("Stiskni 1 pro MP3 ; Stiskni 2 pro Smartphone ; Stiskni 3 pro radio");
-                int volba = sc.nextInt();
-                sc.nextLine();
-
-                switch (volba) {
-                    case 1:
-                        System.out.println("Jaká je značka MP3?");
-                        String znacka = sc.nextLine();
-                        prehravace[i] = new Mp3(znacka);
-                        break;
-                    case 2:
-                        System.out.println("Jaký je model smartphonu");
-                        String model = sc.nextLine();
-                        prehravace[i] = new Smartphone(model);
-                        break;
-                    case 3:
-                        prehravace[i] = new Radio();
-                        break;
-                }
-                break;
+            switch (volba) {
+                case 1:
+                    System.out.println("Jaká je značka MP3?");
+                    String znacka = sc.nextLine();
+                    prehravace[i] = new Mp3(znacka);
+                    break;
+                case 2:
+                    System.out.println("Jaký je model smartphonu?");
+                    String model = sc.nextLine();
+                    prehravace[i] = new Smartphone(model);
+                    break;
+                case 3:
+                    prehravace[i] = new Radio();
+                    break;
+                default:
+                    System.out.println("Neplatná volba. Zkus to znovu.");
+                    i--;
+                    break;
             }
-        case 2:
+        }
 
+        System.out.println("Vyber zařízení pro přehrávání (1 až " + (count + 0) + "):");
+        int prehraj = sc.nextInt();
+        sc.nextLine();
 
-            IPrehravac[] zarizeni = new Mp3[3];
-            zarizeni[0] = new Mp3("Sony");
-            zarizeni[1] = new Smartphone("Appel");
-            zarizeni[2] = new Radio();
+        if (prehraj >= 0 && prehraj < count) {
+            prehravace[prehraj].prehraj(true);
+        } else {
+            System.out.println("Neplatná volba.");
+        }
 
+        System.out.println("Vyber zařízení pro zastavení přehrávání (1 až " + (count + 0) + "):");
+        int zastav = sc.nextInt();
+        sc.nextLine();
 
-            break;
-        //Mp3[] mp3 = new Mp3(znacka){
-        //   prehravace[i] = mp3
-            }
+        if (zastav >= 0 && zastav < count) {
+            prehravace[zastav].prehraj(false);
+        } else {
+            System.out.println("Neplatná volba.");
         }
     }
 }
